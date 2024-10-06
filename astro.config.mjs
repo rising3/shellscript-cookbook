20d05bf7cdc8a138495b5110bb9ef5f917a1ea98
rising3 <michio.nakagawa@gmail.com>
2024-10-06 12:42:02 +0900

    fix: add bash path

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://rising3.github.io/shellscript-cookbook',
+	site: 'https://rising3.github.io',
+	base: '/shellscript-cookbook',
 	integrations: [
 		starlight({
 			title: 'shellscript Cookbook',
