180ce4e4ad4d60691b8c68f4a7b9dc572621dd53
rising3 <michio.nakagawa@gmail.com>
2024-10-06 12:02:04 +0900

    feat: add starlight

---
+++ astro.config.mjs
@@ -0,0 +1,29 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://rising3.github.io/shellscript-cookbook',
+	integrations: [
+		starlight({
+			title: 'shellscript Cookbook',
+			social: {
+				github: 'https://github.com/rising3/shellscript-cookbook',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
